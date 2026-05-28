3f96b6d6c55b5714e15819778f74235cadebe283
asmileyguy <extremelycoolguy09@gmail.com>
2026-05-28 19:00:34 +0300

    Add assembly flags to config.mk

--- userspace/config.mk
+++ userspace/config.mk
@@ -1,5 +1,7 @@
 CC = gcc
 CFLAGS = -Wall -m64 -fno-stack-protector -fPIC -fno-builtin -nostdlib -nostdinc -nodefaultlibs -I./../../include/ -L../../ -L. -MMD -MP -std=gnu99 -march=x86-64 -mtune=generic
+AS = $(CC)
+AFLAGS = $(CFLAGS) -D__ASSEMBLY__
 LD = $(CC)
 LDFLAGS = $(CFLAGS)
 AR = ar
