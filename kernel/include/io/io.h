6761ab22bf5954c7df97002b9076caa62d2d1e32
asmileyguy <amogus@amogus-pc.localdoman>
2026-06-07 14:40:11 +0300

    Update crt0.S to use sys/syscall.h

--- kernel/include/io/io.h
+++ kernel/include/io/io.h
@@ -2,10 +2,10 @@
 
 #include <freestanding/stdint.h>
 
-void outb(uint8_t val, uint16_t port);
+void outb(uint16_t port, uint8_t val);
 uint8_t inb(uint16_t port);
-void outw(uint16_t val, uint16_t port);
+void outw(uint16_t port, uint16_t val);
 uint16_t inw(uint16_t port);
-void outl(uint32_t val, uint16_t port);
+void outl(uint16_t port, uint32_t val);
 uint32_t inl(uint16_t port);
 void io_wait(void);
