7d95467d4a5f44eb8c1edc4892c4e49fd8474231
asmileyguy <amogus@amogus-pc.localdoman>
2026-06-12 09:26:46 +0300

    Update kernel strings.c

--- kernel/main/strings.c
+++ kernel/main/strings.c
@@ -11,6 +11,32 @@ char *rindex(const char *s, int c) {
     return strrchr(s, c);
 }
 
+int bcmp(const void *s1, const void *s2, size_t n) {
+    return memcmp(s1, s2, n);
+}
+
+void bcopy(const void *src, void *dest, size_t n) {
+    memmove(dest, src, n);
+}
+
+void bzero(void *s, size_t n) {
+    memset(s, 0, n);
+}
+
+int ffs(int i) {
+    if (i == 0) {
+        return 0;
+    }
+
+    int bit = 1;
+    unsigned int value = (unsigned int)i;
+    while ((value & 1U) == 0) {
+        value >>= 1;
+        bit++;
+    }
+    return bit;
+}
+
 int strcasecmp(const char *a, const char *b) {
     while (*a && *b) {
         if (toupper(*a) != toupper(*b)) return toupper(*a) - toupper(*b);
