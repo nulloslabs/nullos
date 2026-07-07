511b39474fb8bb94c3a0f2b15e3bada3de1bf7b4
asmileyguy <extremelycoolguy09@gmail.com>
2026-07-07 17:50:23 +0300

    MacOS bugfixes

--- rootfs/config.mk
+++ rootfs/config.mk
@@ -1,7 +1,13 @@
 KERNEL := $(shell uname -s)
 ARCH := $(shell uname -m)
 
+ifeq ($(KERNEL),Linux)
 TAR = tar
 TARFLAGS = -c --format=ustar --owner=0 --group=0 --numeric-owner --use-compress-program='gzip -9'
+else ifeq ($(KERNEL),Darwin)
+TAR = docker run --rm -v "$(PWD)":"$(PWD)" -w "$(PWD)" alpine tar
+TARFLAGS = -c --format=ustar --owner=0 --group=0 --numeric-owner --use-compress-program='gzip -9'
+endif
+
 SUBDIR = rootfs
 OUTFILE = rootfs.tar.gz
