e1f22093413eec776f3400fd708b20047aab404c
asmileyguy <extremelycoolguy09@gmail.com>
2026-08-14 11:17:15 +0300

    Fix empty kernel release

--- kernel/config.mk
+++ kernel/config.mk
@@ -14,7 +14,7 @@ else
 endif
 
 CC = $(CROSS)gcc
-CFLAGS = -O2 -m64 -std=c11 -Wall -Wextra -I../include/ -I../include/freestanding/ -I../include/limine/ -I../uacpi/include/ -DKERNEL_RELEASE=\"$(KERNEL_RELEASE)\" -ffreestanding -nostdlib -nostdinc -fno-builtin -nodefaultlibs -nostartfiles -fstack-protector-strong -mstack-protector-guard=global -fno-pic -fno-pie -no-pie -fno-lto -fno-stack-check -mno-red-zone -mcmodel=kernel -mabi=sysv -march=x86-64 -mtune=generic -mno-sse -mno-sse2 -mno-mmx -mno-80387 -MMD -MP
+CFLAGS := -O2 -m64 -std=c11 -Wall -Wextra -I../include/ -I../include/freestanding/ -I../include/limine/ -I../uacpi/include/ -DKERNEL_RELEASE=\"$(KERNEL_RELEASE)\" -ffreestanding -nostdlib -nostdinc -fno-builtin -nodefaultlibs -nostartfiles -fstack-protector-strong -mstack-protector-guard=global -fno-pic -fno-pie -no-pie -fno-lto -fno-stack-check -mno-red-zone -mcmodel=kernel -mabi=sysv -march=x86-64 -mtune=generic -mno-sse -mno-sse2 -mno-mmx -mno-80387 -MMD -MP
 UACPI_CFLAGS = $(filter-out -I../include/ -I../include/freestanding/ -I../include/limine/ -I../uacpi/include/,$(CFLAGS)) -I./include/ -I./include/freestanding/ -I./include/limine/ -I./uacpi/include
 ifeq ($(DEBUG),1)
 	CFLAGS := -g $(CFLAGS)
