bdff99ea865b64c6682be3ec0ce736c9a7123fc0
asmileyguy <extremelycoolguy09@gmail.com>
2026-09-17 21:28:15 +0300

    Remove alsa-utils package in initrd generation

--- initrd/config.mk
+++ initrd/config.mk
@@ -14,7 +14,7 @@ CURL = curl
 CURLFLAGS = --retry 3 --retry-delay 2 --retry-all-errors
 
 # TODO: Use my own packages instead of Arch packages
-PACKAGE_NAMES := core/glibc core/ncurses core/readline core/bash core/nano extra/busybox extra/alsa-utils
+PACKAGE_NAMES := core/glibc core/ncurses core/readline core/bash core/nano extra/busybox
 
 SUBDIR = initrd
 OUTFILE = initrd.cpio.gz
