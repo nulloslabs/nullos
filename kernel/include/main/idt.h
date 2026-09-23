823db5d5eada3b22431709106377cc97379170e1
asmileyguy <extremelycoolguy09@gmail.com>
2026-09-23 12:32:39 +0300

    Remove useless comments from idt.h

--- kernel/include/main/idt.h
+++ kernel/include/main/idt.h
@@ -2,18 +2,16 @@
 
 #include <stdint.h>
 
-// The IDT entry structure
 struct idt_entry {
-    uint16_t isr_low;   // The lower 16 bits of the ISR's address
-    uint16_t kernel_cs; // The GDT segment selector that the CPU will load before calling the ISR
-    uint8_t ist;       // The IST in the TSS
-    uint8_t attributes;// Type and attributes
-    uint16_t isr_mid;   // The higher 16 bits of the lower 32 bits of the ISR's address
-    uint32_t isr_high;  // The higher 32 bits of the ISR's address
-    uint32_t reserved;  // Set to zero
+    uint16_t isr_low;
+    uint16_t kernel_cs;
+    uint8_t ist;
+    uint8_t attributes;
+    uint16_t isr_mid;
+    uint32_t isr_high;
+    uint32_t reserved;
 } __attribute__((packed));
 
-// The IDTR structure
 struct idt_ptr {
     uint16_t limit;
     uint64_t base;
